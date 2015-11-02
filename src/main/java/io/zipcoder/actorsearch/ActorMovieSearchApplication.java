682978d413e2a630108bf7589ec279e871058f61
Sean Strauss <seandstrauss@gmail.com>
2015-11-02 10:11:56 -0500

    first commit

---
+++ src/main/java/io/zipcoder/actorsearch/ActorMovieSearchApplication.java
@@ -0,0 +1,12 @@
+package io.zipcoder.actorsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorMovieSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorMovieSearchApplication.class, args);
+    }
+}
